2883da452a8d2a1bf4de8ef7dbd974d841b481b1
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2019-10-03 13:28:50 -0700

    workshop Robot

--- src/section2/MyFirstJavaProgram.java
+++ src/section2/MyFirstJavaProgram.java
@@ -8,8 +8,28 @@ public class MyFirstJavaProgram {
 	public static void main(String[] args) {
 		
 		// START HERE
-	
-		
+Robot oreo = new Robot();
+oreo.setSpeed(100);
+oreo.penDown();
+for(int i=0; i<4 ; i++) {
+oreo.move(100);
+oreo.turn(90);
+}	
+oreo.move(200);
+oreo.turn(90);
+oreo.turn(90);
+oreo.move(200);
+oreo.move(200);
+oreo.turn(90);
+oreo.move(200);
+oreo.sparkle();
+oreo.turn(90);
+oreo.move(200);
+oreo.move(200);
+oreo.turn(90);
+oreo.move(200);
+
+
 		
 	}
 }
